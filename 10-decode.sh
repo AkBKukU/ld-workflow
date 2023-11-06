adb30409425c0ff8f113c6f087c619e92a164db8
Shelby Jueden <shelby@jueden.org>
2023-11-06 11:48:07 -0700

    Update 10-decode.sh

--- 10-decode.sh
+++ 10-decode.sh
@@ -4,9 +4,11 @@ mkdir -p log
 if [[ "" = "$1" ]]
 then
     start=500
+    frame="-S 1"
 else
     start="$1"
+				 frame""
 fi
 
 
-time ld-decode --AC3 --start "$start" -S 1 -t 6 *.lds output 2>&1 | tee -a log/ld-decode.log
+time ld-decode --AC3 --start "$start" $frame -t 6 *.lds output 2>&1 | tee -a log/ld-decode.log
