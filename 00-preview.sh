03e67f90bab3cb1a5fcf688fecfaff24c4b50d1b
Shelby Jueden <shelby@jueden.org>
2023-11-07 08:02:21 -0700

    Add frame start to TBC files

--- 00-preview.sh
+++ 00-preview.sh
@@ -4,9 +4,18 @@ mkdir -p preview
 
 if [[ "" = "$1" ]]
 then
-    start=400
+    frame_start="1"
 else
-    start="$1"
+    frame_start="$1"
 fi
 
-time ld-decode --AC3 -s "$start" -S 1 -l 250 -t 6 *.lds preview/preview 2>&1 | tee -a log/preview.log
+
+if [[ "" = "$2" ]]
+then
+    start=500
+else
+    start="$2"
+fi
+
+
+time ld-decode --AC3 -s "$start" -S $frame_start -l 250 -t 6 *.lds preview/preview 2>&1 | tee -a log/preview.log
