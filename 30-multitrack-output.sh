06b50874d5c453713bc02e408e7d25d341d44190
Shelby Jueden <shelby@jueden.org>
2023-11-08 07:19:09 -0700

    Fix sample rate syncing and cutting off end

--- 30-multitrack-output.sh
+++ 30-multitrack-output.sh
@@ -2,7 +2,8 @@
 
 input="output.tbc"
 #vcodec="-c:v v210 -colorspace smpte170m -color_range tv -pix_fmt yuv422p10le"
-vcodec="-c:v libx265"
+vcodec="-c:v libx265 -crf 20 -preset medium "
+#vcodec="-c copy"
 stereo="pcm_s16le"
 
 # Video filtering
@@ -10,6 +11,7 @@ stereo="pcm_s16le"
 deinterlace=",dedot=m=rainbows,fieldmatch=order=auto:field=auto,decimate" # 24 to 30 correction
 scale=",scale=2*iw:2*ih:flags=neighbor"
 vfilter="-vf setdar=4/3,setfield=tff$deinterlace$scale"
+#vfilter=""
 
 # Comment out this like to not include the original AC3 track in the output
 #ac3pass="-map 2:a:0 -metadata:s:a:3 title=AC3-Surround -c:a:3 ac3 "
@@ -21,9 +23,9 @@ function encode_ac3
 # channel may contain a mono track or something else like commentary.
 # There may be matrix encoded dolby surround in the digital track
     ld-chroma-decoder --decoder ntsc3d -p y4m -q "$input" | ffmpeg -y -i - \
-    -f s16le -r 44.1k -ac 2 -i digital-audio.pcm \
-    -i *.ac3 \
-    -f s16le -r 44.1k -ac 2 -i output.pcm \
+    -f s16le -ar 44.1k -ac 2 -i digital-audio.pcm \
+    -itsoffset 0.19 -i *.ac3 \
+    -f s16le -ar 44086 -ac 2 -i output.pcm \
     -i ffdata \
     -i cc.srt \
     $vcodec $vfilter \
@@ -34,16 +36,16 @@ function encode_ac3
     -map 2:a:0 \
     -metadata:s:a:1 title="PCM Surround" \
     -channel_layout:a:1 "5.1" \
-    -c:a:1 pcm_s16le -osr 44100 \
+    -c:a:1 pcm_s16le \
     -map 3:a:0 \
     -metadata:s:a:2 title="Analog Left Channel" \
-    -c:a:2 $stereo  \
+    -c:a:2 $stereo -ar 44100 \
     -map_channel 3.0.0 \
     $ac3pass \
     -map_metadata 4 \
     -map 5:s:0 \
     -c:s mov_text -metadata:s:s:0 language=eng \
-    -shortest output.mov  2>&1 | tee -a log/ffmpeg.log
+    output.mov  2>&1 | tee -a log/ffmpeg.log
 
 }
 
