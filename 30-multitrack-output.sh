b1527f0d3dd7999940716b69365f020a5aba0b01
Shelby Jueden <shelby@jueden.org>
2023-11-08 16:39:01 -0700

    Add filter option for 60 vs 24 fps deinterlacing

--- 30-multitrack-output.sh
+++ 30-multitrack-output.sh
@@ -7,11 +7,18 @@ vcodec="-c:v libx265 -crf 20 -preset medium "
 stereo="pcm_s16le"
 
 # Video filtering
-#deinterlace=",dedot=m=rainbows,yadif=mode=send_field:parity=auto" # 30 to 60 correction
-deinterlace=",dedot=m=rainbows,fieldmatch=order=auto:field=auto,decimate" # 24 to 30 correction
+if [[ "60" == "$1" ]]
+then
+    echo "60 FPS Deinterlacing"
+    deinterlace=",dedot=m=rainbows,yadif=mode=send_field:parity=auto" # 30 to 60 correction
+else
+    echo "24 FPS Deinterlacing"
+    deinterlace=",dedot=m=rainbows,fieldmatch=order=auto:field=auto,decimate" # 24 to 30 correction
+fi
 scale=",scale=2*iw:2*ih:flags=neighbor"
 vfilter="-vf setdar=4/3,setfield=tff$deinterlace$scale"
 #vfilter=""
+analog_rate=44089
 
 # Comment out this like to not include the original AC3 track in the output
 #ac3pass="-map 2:a:0 -metadata:s:a:3 title=AC3-Surround -c:a:3 ac3 "
@@ -25,7 +32,7 @@ function encode_ac3
     ld-chroma-decoder --decoder ntsc3d -p y4m -q "$input" | ffmpeg -y -i - \
     -f s16le -ar 44.1k -ac 2 -i digital-audio.pcm \
     -itsoffset 0.19 -i *.ac3 \
-    -f s16le -ar 44086 -ac 2 -i output.pcm \
+    -f s16le -ar $analog_rate -ac 2 -i output.pcm \
     -i ffdata \
     -i cc.srt \
     $vcodec $vfilter \
@@ -55,8 +62,8 @@ function encode_dstereo_split
 # Digital stereo discs will contain two stereo tracks
 # There may be matrix encoded dolby surround in the digital track
     ld-chroma-decoder --decoder ntsc3d -p y4m -q "$input" | ffmpeg -y -i - \
-    -f s16le -r 44.1k -ac 2 -i digital-audio.pcm \
-    -f s16le -r 44.1k -ac 2 -i output.pcm \
+    -f s16le -ar 44.1k -ac 2 -i digital-audio.pcm \
+    -f s16le -ar $analog_rate -ac 2 -i output.pcm \
     -i ffdata \
     $vcodec $vfilter \
     -map 0:v:0 \
@@ -79,19 +86,20 @@ function encode_dstereo
 # Digital stereo discs will contain two stereo tracks
 # There may be matrix encoded dolby surround in the digital track
     ld-chroma-decoder --decoder ntsc3d -p y4m -q "$input" | ffmpeg -y -i - \
-    -f s16le -r 44.1k -ac 2 -i digital-audio.pcm \
-    -f s16le -r 44.1k -ac 2 -i output.pcm \
+    -f s16le -ar 44.1k -ac 2 -i digital-audio.pcm \
+    -f s16le -ar $analog_rate -ac 2 -i output.pcm \
     -i ffdata \
-    $vcodec $vfilter \
     -map 0:v:0 \
+    $vcodec $vfilter \
     -map 1:a:0 \
     -metadata:s:a:0 title="Digital Stereo" \
     -c:a:0 $stereo  \
     -map 2:a:0 \
     -metadata:s:a:1 title="Analog Stereo" \
-    -c:a:3 $stereo  \
+    -c:a:1 $stereo -ar 44100 \
     -map_metadata 3 \
     output.mov  2>&1 | tee -a log/ffmpeg.log
+
 }
 
 
@@ -99,7 +107,7 @@ function encode_astereo
 {
 # Non-digital sound discs will contain analog stereo audio
     ld-chroma-decoder --decoder ntsc3d -p y4m -q "$input" | ffmpeg -y -i - \
-    -f s16le -r 44.1k -ac 2 -i output.pcm \
+    -f s16le -ar $analog_rate -ac 2 -i output.pcm \
     -i ffdata \
     $vcodec $vfilter\
     -map 0:v:0 \
